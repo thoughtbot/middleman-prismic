64db4e663c92db97da3209d264273bcc17f53f0b
Ian Zabel <ian@thoughtbot.com>
2018-09-18 16:13:42 -0400

    Add support for link_resolver for preview redirect

--- lib/middleman-prismic/preview.rb
+++ lib/middleman-prismic/preview.rb
@@ -3,6 +3,7 @@ module Middleman
     class Preview
       def initialize(app, options={})
         @app = app
+        @options = options
       end
 
       def call(env)
@@ -10,14 +11,23 @@ module Middleman
         if req.path =~ %r(^/preview)
           token = req.params["token"]
 
-          succeeded = Kernel.system "middleman", "prismic", "--ref", token
+          succeeded = Kernel.system 'middleman', 'prismic', '--ref', token
           if succeeded
-            [302, {'Location' => '/'}, ['Found']]
+            [302, {'Location' => preview_url(token)}, ['Found']]
           else
             [500, {'Location' => '/?error=preview_failure'}, ['Error']]
           end
         else
           @app.call(env)
+        end
+      end
+
+      def preview_url(token)
+        if @options[:api_url] && @options[:link_resolver]
+          api = ::Prismic.api(@options[:api_url])
+          api.preview_session(token, @options[:link_resolver], '/')
+        else
+          '/'
         end
       end
     end
